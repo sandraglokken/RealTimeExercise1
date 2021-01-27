9794a5a5c32911baa95335f75d04b15a353220e8
Sandra Garder Løkken <sandrgl@stud.ntnu.no>
2021-01-27 15:53:17 +0100

    not finding pthread header file

--- c/foo.c
+++ c/foo.c
@@ -8,17 +8,26 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for(int count = 0; count<1000000; count++){
+        i++;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for(int count = 0; count<1000000; count++){
+        i--;
+    }
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
